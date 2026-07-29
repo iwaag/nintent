5743c6a63b0d1240c8832bd4d5710911a0d6206f
iwaag <dummy@mail.com>
2026-07-29 19:45:18 +0900

    fix: merge gateway and dashboard migrations

---
+++ nautobot_intent_catalog/migrations/0017_merge_20260729_1945.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("nautobot_intent_catalog", "0016_desiredendpoint_gateway_address"),
+        ("nautobot_intent_catalog", "0016_remove_reconciliation_dashboard_surfaces"),
+    ]
+
+    operations = []
